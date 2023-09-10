a530c8082b851b1b614caf7bb4eac7a6a8b708ed
Ben Carter <ben.carter@three-ships.com>
2023-09-10 03:03:42 -0400

    Added sample module

---
+++ modules/index.ts
@@ -0,0 +1,2 @@
+/* Make sure not to use the full path aka hello.ts if will not transpile correctly. */
+import './server/hello';
---
+++ modules/server/hello.ts
@@ -0,0 +1,25 @@
+/**
+ * Will say hello to the world when the command .hello is entered.
+ * @event PLAYER_EVENT_ON_COMMAND 
+ */
+ class Hello {
+    onCommand: player_event_on_command = (
+      event: number,
+      player: Player,
+      command: string
+    ) => {
+      if (command == "hello") {
+        const message = "Hello World command was entered!";
+        
+        PrintInfo(message);
+        SendWorldMessage(message);
+      }    
+      return true;
+    };
+  }
+  
+  const hello = new Hello();
+  RegisterPlayerEvent(PlayerEvents.PLAYER_EVENT_ON_COMMAND, (...args) =>
+    hello.onCommand(...args)
+  );
+  
